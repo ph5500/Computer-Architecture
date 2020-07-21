ea890a229a28c8c328e344d988c383a343175223
ph5500 <philfives@gmail.com>
2020-07-21 16:36:35 -0600

    Day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,12 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.branch_table = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100010: self.MUL,
+        }
         
     def ram_read(self, mar):
         return self.ram[mar]
@@ -18,26 +24,38 @@ class CPU:
         # mdr is the data being written
         self.ram[mar] = mdr
 
-    def load(self):
+    def load(self, filename):
+    
         """Load a program into memory."""
 
         address = 0
 
+        
+        with open(filename) as f:
+            for line in f:
+                line = line.split('#')
+                try:
+                    v = int(line[0], 2)
+                except ValueError:
+                    continue
+                self.ram[address] = v
+                address += 1
+                
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -68,15 +86,32 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+        
+    def HLT(self, operand_a, operand_b):
+        sys.exit()
+    
+    def LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+        
+    def PRN(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2    
+        
+    def MUL(self, operand_a, operand_b):
+        self.reg[operand_a] *= self.reg[operand_b]
+        self.pc += 3
+        
+    
 # 1, 2, 4, 8, 16, 32, 64, 128 etc.
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        running = True
+        # HLT = 0b00000001
+        # LDI = 0b10000010
+        # PRN = 0b01000111
+        # running = True
         
-        while running:
+        while True:
             # need to read the memory address that's store in the register PC
             # store the result in the ir
             ir = self.ram_read(self.pc)
@@ -84,16 +119,18 @@ class CPU:
             operand_a = self.ram_read(self.pc +1)
             operand_b = self.ram_read(self.pc +2)
             
-            if ir == HLT:
-                running = False
-            elif ir == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif ir == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
+            # if ir == HLT:
+            #     running = False
+            # elif ir == LDI:
+            #     self.reg[operand_a] = operand_b
+            #     self.pc += 3
+            # elif ir == PRN:
+            #     print(self.reg[operand_a])
+            #     self.pc += 2
+            if ir in self.branch_table:
+                self.branch_table[ir](operand_a, operand_b)
             else: 
                 print(f'Unkown instruction {ir} at address {self.pc}')
                 # stop the program by calling it in the run function
-                running = False
+                # running = False
                 sys.exit()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,11 @@
 import sys
 from cpu import *
 
+
+filename = sys.argv[1]
+
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(filename)
 cpu.run()
